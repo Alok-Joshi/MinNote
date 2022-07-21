12febd0a3eb84ea522d492fe28dd7cfdf1a043f6
Alok Joshi <AlokJoshi523@gmail.com>
2022-07-21 15:47:51 +0530

    minor changes

--- MinNote/notes/models.py
+++ MinNote/notes/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Note(models.Model):
-	user_id = models.ForeignKey(
+	#user_id = models.ForeignKey(
 	title = models.CharField(max_length = 25)
 	content = models.CharField(max_length = 1000) 
 
--- MinNote/notes/views.py
+++ MinNote/notes/views.py
@@ -4,9 +4,10 @@ from django.http import HttpResponse
 
 def view_all_notes(request):
 	""" Loads the main page of the app, displays all the notes """
+	#will first authenticate (use login required decorator or do it yourself), then use templates to render notes page 
 
 def delete_note(request,note_id):
-	""" deletes a note and redirects to the main page """
+	""" deletes a note and refreshes the main page """
 
 def edit_note(request,note_id):
 	""" edits a note and redirects to the main page """
@@ -14,4 +15,3 @@ def edit_note(request,note_id):
 def add_note(request):
 	""" Adds a note and redirects to the main page """ 
 
-# Create your views here.
